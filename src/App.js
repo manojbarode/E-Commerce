5a23171684e9ceefa1edcfc00f1a473ad7f661b2
manojbarode <your-email@example.com>
2025-11-20 13:32:55 +0530

    login

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Footer from './components/Footer';
 import Home from './pages/Home.jsx';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout.jsx';
+import Login from './pages/Login.jsx';
 let App=()=>{
   return(
     <>
       <Routes>
       {/* Layout wraps all pages with Navbar + Footer */}
-      <Route element={<Layout />}>
+        <Route element={<Layout />}/>
         {/* Add more routes as needed */}
         <Route path="/" element={<Home />} />
-      </Route>
+        <Route path="/Login" element={<Login/>}/>
+      
     </Routes>
     </>
   )
---
+++ src/pages/Login.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Login=()=>{
+    <>
+    <h1>hello</h1>
+    </>
+}
+export default Login;
