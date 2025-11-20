ca3c4a2500d9d1fe9980bcc013e0091f22e1fd4b
deepakdawar07 <dawardeepak2512.com>
2025-11-20 17:12:40 +0530

    profile added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Signup from "./pages/Signup";
 
 // AuthContext import
 import AuthProvider from "./context/AuthContext";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile/>}/>
         </Route>
       </Routes>
     </AuthProvider>
