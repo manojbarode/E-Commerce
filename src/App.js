5328a7905e7f4383216ebd774e69b4304bbe2fd9
deepakdawar07 <dawardeepak2512.com>
2025-11-21 12:17:53 +0530

    logoutoption

--- src/App.js
+++ src/App.js
@@ -4,20 +4,35 @@ import { Route, Routes } from "react-router-dom";
 
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import Login from "./pages/Login"; 
-import Signup from "./pages/Signup"; 
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
-// AuthContext import
+// User Profile Page (Add this import)
+import Profile from "./pages/Profile";
+
+// Auth Context
 import AuthProvider from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
     <AuthProvider>
       <Routes>
+        {/* Layout for all normal pages */}
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+
+          {/* Protected Route for Profile */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
         </Route>
       </Routes>
     </AuthProvider>
