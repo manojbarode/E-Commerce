0400ba97de314b69f345891395df7ffa7d0a2fb5
manojbarode <your-email@example.com>
2025-11-24 13:00:17 +0530

    j

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import { Route, Routes } from "react-router-dom";
-
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Profile from "./pages/Profile";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-import AuthProvider from "./context/AuthContext";
-
-const App = () => {
-  return (
-    <AuthProvider>
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-
-          {/* Protected profile route */}
-          <Route
-            path="/profile"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-      </Routes>
-    </AuthProvider>
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout.jsx';
+import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx';
+let App=()=>{
+  return(
+    <Routes>
+        <Route element={<Layout />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login/>}/>
+      
+    </Routes>
   );
-};
-
-export default App;
+}
+export default App
