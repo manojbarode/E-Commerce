1a567aab9551c5741cd468e29f3d20d7e9500622
manojbarode <your-email@example.com>
2025-11-24 13:09:13 +0530

    all changed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
@@ -32,6 +33,23 @@ const App = () => {
         </Route>
       </Routes>
     </AuthProvider>
+=======
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout.jsx';
+import Login from './pages/Login.jsx';
+import Home from './pages/Home.jsx';
+let App=()=>{
+  return(
+    <Routes>
+        <Route element={<Layout />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login/>}/>
+      
+    </Routes>
+>>>>>>> 68ef7ca (not)
   );
 };
 
