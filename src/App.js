bbf4f21658d09762d1545d19b8a831f85ddb41cd
manojbarode <your-email@example.com>
2025-11-28 13:15:17 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Login from "./pages/Login";
 import Addseller from "./pages/Admin/Addseller";
 import HelpDesk from "./pages/help";
+import Offers from "./pages/Offers";
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
         <Route path="/signup" element={<Signup />} />
         <Route path="/Addseller" element={<Addseller />} />
         <Route path="/help" element={<HelpDesk />}/>
+        <Route path="/offers" element={<Offers/>}/>
         <Route path="/profile" element={
             <ProtectedRoute>
               <Profile />
