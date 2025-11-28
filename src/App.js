6c0a78723d9d0b50e3080f26741224efe8086299
deepakdawar07 <dawardeepak2512.com>
2025-11-28 15:05:38 +0530

    offeres added

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import HelpDesk from "./pages/help";
 import ProductUpload from "./pages/Seller/ProductUpload";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 import SellerDashboard from "./pages/Seller/SellerDashboard1/SellerDashboard";
+import OffersPage from "./pages/Offers";
 
 
 
@@ -24,6 +25,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/Addseller" element={<Addseller />} />
+        <Route path="/offers" element={<OffersPage/>}/>
         <Route path="/help" element={<HelpDesk />}/>
         <Route path="/dashboard" element={<AdminDashboard/>}/>
         <Route path="/sellerdashboard" element={<SellerDashboard/>}/>
