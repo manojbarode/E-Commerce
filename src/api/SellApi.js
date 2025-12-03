450c8d539878221608c0a38c9f2f014bf3108b87
deepakdawar07 <dawardeepak2512.com>
2025-12-03 13:16:20 +0530

    dashboard api added

--- src/api/SellApi.js
+++ src/api/SellApi.js
@@ -14,7 +14,42 @@ export const loginSeller = async (credentials) => {
   try {
     const response = await axiosInstance.post("/seller/login", credentials);
     return response.data;
-  } catch (error) {
+  } 
+  catch (error)
+  {
+    throw error.response?.data || error.message;
+  }
+};
+
+export const getSellerStats = async(sellerId)=>{
+  try
+  {
+    const response = await axiosInstance.get(`/seller/${sellerId}/stats`);
+    return response.data;
+  }
+  catch(error){
+    throw error.response?.data || error.message;
+  }
+};
+
+export const getMonthlySales =  async(sellerId)=>{
+  try
+  {
+    const response = await axiosInstance.get(`/seller/${sellerId}/sales`);
+    return response.data;
+  }
+  catch(error){
+    throw error.response?.data || error.message;
+  }
+};
+
+export const getCategoryStats = async(sellerId)=>{
+  try 
+  {
+    const res = await axiosInstance.get(`/seller/${sellerId}/category-stats`);
+    return res.data;
+  } 
+  catch (error) {
     throw error.response?.data || error.message;
   }
 };
